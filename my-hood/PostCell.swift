80ce976d1622ed73a351f32aec78c88031889cba
Daniel J Janiak <danieljjaniak@daniels-air.uwyo.edu>
2016-05-26 16:14:49 -0600

    Create a custom table view cell class

---
+++ my-hood/PostCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCell.swift
+//  my-hood
+//
+//  Created by Daniel J Janiak on 5/26/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
