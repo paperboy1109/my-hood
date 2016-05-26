0b41f94dc85d87045cd0312048646479ae8a0e4f
Daniel J Janiak <danieljjaniak@daniels-air.uwyo.edu>
2016-05-26 14:46:41 -0600

    Name the main view controller 'PostsViewController' and make it a delegate and data source for the table view

---
+++ my-hood/PostsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  my-hood
+//
+//  Created by Daniel J Janiak on 5/26/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 87.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+
+
+
+
+}
+
