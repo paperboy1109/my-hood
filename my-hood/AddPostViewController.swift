296dd1eebf26b7412f692f0a9d99fc5400c1c963
Daniel J Janiak <danieljjaniak@daniels-air.uwyo.edu>
2016-05-26 16:53:15 -0600

    Create a view controller and UI for making new posts

---
+++ my-hood/AddPostViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddPostViewController.swift
+//  my-hood
+//
+//  Created by Daniel J Janiak on 5/26/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class AddPostViewController: UIViewController {
+
+}
