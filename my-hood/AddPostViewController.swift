2a7c3f63475bdba0d131b798948b0bbd0cbe75a6
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-05-27 16:40:23 -0600

    Allow user to pick an image

--- my-hood/AddPostViewController.swift
+++ my-hood/AddPostViewController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class AddPostViewController: UIViewController {
+class AddPostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet var titleField: UITextField!
     @IBOutlet var descField: UITextField!
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         postImg.layer.cornerRadius = postImg.frame.size.width / 2
         postImg.clipsToBounds = true
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
     
     @IBAction func makePostBtnTapped(sender: AnyObject) {
-        
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            
+        }
     }
     
     @IBAction func cancelBtnTapped(sender: AnyObject) {
@@ -31,10 +38,16 @@ class AddPostViewController: UIViewController {
     
     }
     
-    @IBAction func adPicBtnTapped(sender: UIButton!) {
+    @IBAction func addPicBtnTapped(sender: UIButton!) {
         
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
         
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
     }
 
 }
